7d62d3fe20765f1f335b5c45149d28a009ac12f9
unknown <axmedxabiib2018@gmail.com>
2020-06-13 10:04:40 +0300

    updaated app.js 2

--- app.js
+++ app.js
@@ -13,3 +13,29 @@
 			<a class="btn btn-primary" href="/campgrounds/new" > Add New Campground</a>
 		</div>
 	</header>
+    	
+	<div class="row text-center" style="display:flex; flex-wrap: wrap">
+		<% campgrounds.forEach(function(campground){  %>
+		<div class="col-md-3 col-sm-6">
+			<div class="thumbnail">
+				<img src="<%= campground.image %>">
+				<div class="caption">
+					<h4><%= campground.name %></h4>
+				</div>
+				<p>
+					<a href="/campgrounds/<%= campground._id %>" class="btn btn-primary">More Info</a>
+				</p>
+
+			</div>
+			
+		</div>
+		<% }) %>
+	</div>
+</div>
+var express         = require("express"),
+    router          = express.Router(),
+	Campground      = require("../models/campground"),
+	middlewareObj	= require("../middleware");
+
+
+<%- include("../partials/footer") %>
